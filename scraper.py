d4b02b296fdaf3bca2f29e9b7d3e82a9e526efb6
Supreetha H M <supreethahm517@gmail.com>
2026-01-14 14:45:59 +0530

    Configure for Vercel deployment

--- scraper.py
+++ scraper.py
@@ -6,9 +6,8 @@ import json
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
-
 # Inside scraper.py
-OUTPUT_DIR = "/tmp/sku"
+OUTPUT_DIR = "sku"
 
 
 # Main folder as requested
